be5044f16d2e20736e8586c78ff83bce65979248
Rin <khongnhoten312@gmail.com>
2020-10-27 23:22:14 +0700

    linked_list

--- Java/Linkedlist/src/com/company/Main.java
+++ Java/Linkedlist/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     static class SinglyLinkedListNode{
         public int data ;
@@ -10,9 +12,41 @@ public class Main {
         }
     }
     static class SinglyLinkedList{
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+        public SinglyLinkedList(){
+            this.head = null;
+            this.tail = null;
+        }
+        public void insertNode(int nodeData){
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+            if(this.head==null){
+                this.head=node;
+            }
+            else{
+                this.tail.next = node;
+            }
+            this.tail=node;
+        }
 
     }
+    // in danh sách liên kết
+    public static void print(SinglyLinkedListNode head){
+        SinglyLinkedListNode node = head;
+        while (node!=null){
+            System.out.println(node.data);
+            node=node.next;
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
+        SinglyLinkedList list = new SinglyLinkedList();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i= 0;i<n;i++){
+            list.insertNode(scanner.nextInt());
+        }
+        print(list.head);
     }
 }
