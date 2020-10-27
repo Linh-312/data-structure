f7a1e598a12b8bf1849a6e1fb39409b3e1d658a3
Rin <khongnhoten312@gmail.com>
2020-10-27 23:06:07 +0700

    .

---
+++ Java/Linkedlist/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+    static class SinglyLinkedListNode{
+        public int data ;
+        public SinglyLinkedListNode next;
+        public SinglyLinkedListNode(int nodeData){
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+    static class SinglyLinkedList{
+
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
