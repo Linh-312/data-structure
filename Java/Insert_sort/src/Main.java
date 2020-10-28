7981671c8ab00cbec2c1c225c5db4d38e0618fa5
Rin <khongnhoten312@gmail.com>
2020-10-28 12:06:43 +0700

    Insert_sort

--- Java/Insert_sort/src/Main.java
+++ Java/Insert_sort/src/Main.java
@@ -1,8 +1,33 @@
 public class Main {
-    public static void sortInsert(int arr[]){
-        
+    public static void sortInsert(int [] arr){
+        for (int i = 0 ;i < arr.length-1;i++){
+            int pot = arr[i+1];
+            for (int j = i;j > -1;j--){
+                if(arr[j] > pot){
+                    arr[j+1] = arr[j];
+                    if(pot < arr[j] && j==0){
+                        arr[0]=pot;
+
+                    }
+                }
+                else {
+                    arr[j+1] = pot;
+                    break;
+                }
+            }
+            in(arr);
+        }
+    }
+    public static void in(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
     }
     public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 5, 6, 2};
+        sortInsert(arr);
 
     }
 }
--- Java/Test/src/com/company/Main.java
+++ Java/Test/src/com/company/Main.java
@@ -1,33 +1,34 @@
 package com.company;
 
 public class Main {
-    static class SingLinkListNode{
-        public int data;
-        public SingLinkListNode next;
-        public SingLinkListNode(int nodeData){
-            this.data = nodeData;
-            this.next = null;
+    public static void insertionSort2(int n, int[] arr) {
+        for(int i = 0;i<arr.length-1;i++){
+            int k = arr[i+1];
+            for(int j=i;j>-1;j--){
+                if(arr[j] > k){
+                    arr[j+1] = arr[j];
+                    if(arr[j] > k&&j==0){
+                        arr[j+1]=arr[j];
+                        arr[0]=k;
+                    }
+                }
+                else {
+                    arr[j+1]=k;
+                    break;
+                }
+            }
+            in(arr);
         }
     }
-    static class SingLinkList{
-        public SingLinkListNode head;
-        public SingLinkListNode tail;
-        public SingLinkList(){
-            this.head = null;
-            this.tail = null;
+    public static void in(int []arr){
+        for(int i = 0 ;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
-        public void insertNode(int nodeData){
-            SingLinkListNode node = new SingLinkListNode(nodeData);
-            if (this.head == null){
-                this.head = node;
-            }
-            else {
-                this.tail.next=node;
-            }
-            this.tail=node;
-        }
+        System.out.print("\n");
     }
     public static void main(String[] args) {
 	// write your code here
+        int[] arr = {1,9,0,2,4,3};
+        insertionSort2(arr.length,arr);
     }
 }
