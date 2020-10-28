77585c42734b7b1568a803922b927dbd7e91cbb8
Linh-312 <70009274+Linh-312@users.noreply.github.com>
2020-10-28 12:10:41 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,52 +1,33 @@
-package com.company;
-
-import java.util.Scanner;
-
 public class Main {
-    static class SinglyLinkedListNode{
-        public int data ;
-        public SinglyLinkedListNode next;
-        public SinglyLinkedListNode(int nodeData){
-            this.data = nodeData;
-            this.next = null;
+    public static void sortInsert(int [] arr){
+        for (int i = 0 ;i < arr.length-1;i++){
+            int pot = arr[i+1];
+            for (int j = i;j > -1;j--){
+                if(arr[j] > pot){
+                    arr[j+1] = arr[j];
+                    if(pot < arr[j] && j==0){
+                        arr[0]=pot;
+
+                    }
+                }
+                else {
+                    arr[j+1] = pot;
+                    break;
+                }
+            }
+            in(arr);
         }
     }
-    static class SinglyLinkedList{
-        public SinglyLinkedListNode head;
-        public SinglyLinkedListNode tail;
-        public SinglyLinkedList(){
-            this.head = null;
-            this.tail = null;
+    public static void in(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
         }
-        public void insertNode(int nodeData){
-            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
-            if(this.head==null){
-                this.head=node;
-            }
-            else{
-                this.tail.next = node;
-            }
-            this.tail=node;
-        }
+        System.out.println();
 
     }
-    // in danh sách liên kết
-    public static void print(SinglyLinkedListNode head){
-        SinglyLinkedListNode node = head;
-        while (node!=null){
-            System.out.println(node.data);
-            node=node.next;
-        }
-    }
-
     public static void main(String[] args) {
-	// write your code here
-        SinglyLinkedList list = new SinglyLinkedList();
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for (int i= 0;i<n;i++){
-            list.insertNode(scanner.nextInt());
-        }
-        print(list.head);
+        int[] arr = {1, 4, 3, 5, 6, 2};
+        sortInsert(arr);
+
     }
 }
